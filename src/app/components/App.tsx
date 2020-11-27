7ce0bfc670429a8cc37f9a7aedd12ced2cb50e9a
maecapozzi <maecapozzi@gmail.com>
2020-11-26 23:37:19 -0500

    Formatting

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,50 +1,55 @@
-import * as React from 'react';
-import axios from 'axios';
-import '../styles/ui.css';
+import * as React from "react";
+import axios from "axios";
+import "../styles/ui.css";
 
 const App = ({}) => {
-    const syncVersion = React.useCallback(() => {
-        const endpoint = 'http://localhost:3000/';
+  const syncVersion = React.useCallback(() => {
+    const endpoint = "http://localhost:3000/";
 
-        axios({
-            method: 'post',
-            url: endpoint,
-        })
-            .then((response) => {
-                if (response.status === 200) {
-                    parent.postMessage({pluginMessage: {type: 'semantic-version', data: response.data}}, '*');
-                }
-            })
-            .catch((error) => console.log(error));
-    }, []);
+    axios({
+      method: "post",
+      url: endpoint,
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          parent.postMessage(
+            {
+              pluginMessage: { type: "semantic-version", data: response.data },
+            },
+            "*"
+          );
+        }
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-    const onCancel = React.useCallback(() => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-    }, []);
+  const onCancel = React.useCallback(() => {
+    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+  }, []);
 
-    React.useEffect(() => {
-        // This is how we read messages sent from the plugin controller
-        window.onmessage = (event) => {
-            const {type, message} = event.data.pluginMessage;
-            if (type === 'semantic-version') {
-                console.log(`Figma Says: ${message}`);
-            }
-        };
-    }, []);
+  React.useEffect(() => {
+    // This is how we read messages sent from the plugin controller
+    window.onmessage = (event) => {
+      const { type, message } = event.data.pluginMessage;
+      if (type === "semantic-version") {
+        console.log(`Figma Says: ${message}`);
+      }
+    };
+  }, []);
 
-    return (
-        <div>
-            <button
-                id="create"
-                onClick={() => {
-                    syncVersion();
-                }}
-            >
-                Sync version
-            </button>
-            <button onClick={onCancel}>Cancel</button>
-        </div>
-    );
+  return (
+    <div>
+      <button
+        id="create"
+        onClick={() => {
+          syncVersion();
+        }}
+      >
+        Sync version
+      </button>
+      <button onClick={onCancel}>Cancel</button>
+    </div>
+  );
 };
 
 export default App;
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,35 +1,35 @@
 figma.showUI(__html__);
 
 figma.ui.onmessage = async (msg) => {
-    if (msg.type === 'semantic-version') {
-        let textNode;
-        const traverse = (node: any) => {
-            if (node.name === 'SemanticVersion') {
-                textNode = node;
-            }
+  if (msg.type === "semantic-version") {
+    let textNode;
+    const traverse = (node: any) => {
+      if (node.name === "SemanticVersion") {
+        textNode = node;
+      }
 
-            if (node.children) {
-                for (var i = 0; i < node.children.length; i++) {
-                    if (node.children[i].name === 'SemanticVersion') {
-                        textNode = node.children[i];
-                    }
+      if (node.children) {
+        for (var i = 0; i < node.children.length; i++) {
+          if (node.children[i].name === "SemanticVersion") {
+            textNode = node.children[i];
+          }
 
-                    traverse(node.children[i]);
-                }
-            }
-        };
+          traverse(node.children[i]);
+        }
+      }
+    };
 
-        traverse(figma.root);
-        const node = figma.getNodeById(textNode.id) as TextNode;
-        await figma.loadFontAsync({family: 'Helvetica Neue', style: 'Bold'});
-        node.characters = `v${msg.data.data.version}`;
+    traverse(figma.root);
+    const node = figma.getNodeById(textNode.id) as TextNode;
+    await figma.loadFontAsync({ family: "Helvetica Neue", style: "Bold" });
+    node.characters = `v${msg.data.data.version}`;
 
-        // This is how figma responds back to the ui
-        figma.ui.postMessage({
-            type: 'semantic-version',
-            message: `Updated the cover page with the new version number: ${msg.data.data.version}`,
-        });
-    }
+    // This is how figma responds back to the ui
+    figma.ui.postMessage({
+      type: "semantic-version",
+      message: `Updated the cover page with the new version number: ${msg.data.data.version}`,
+    });
+  }
 
-    figma.closePlugin();
+  figma.closePlugin();
 };
